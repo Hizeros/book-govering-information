1966019b6bc161f1a0d85da7fc7c8779137987fc
Hizeros <3290606026@qq.com>
2023-04-15 10:11:18 +0800

    feat：定义字典存储数据，增加图书时引用cache

--- library.py
+++ library.py
@@ -1,7 +1,23 @@
 #!/user/bin/python
 # -*- coding: utf-8 -*-
+def gen_request(request):
+    '''
+    1.图书信息基本操作：增加、删除、修改、查询、展示所有图书信息
+    2.通过文件存储图书信息
+    3.图书具有作者、书名、出版社等属性
+    '''
+    pass
 
 # 创建一个空字典用于接收缓存的图书数据
+from cushy_storage import CushyDict
+
+cache = CushyDict('./data')
+cache['book_list'] = [
+                      {"id_book":1,"name_book":"大一下语文","author":"xiaolai"},
+                      {"id_book":2,"name_book":"大一下数学","author":"zinan"}
+                     ]
+
+
 
 
 
@@ -15,18 +31,26 @@ def _get_book_id(book_id):
     return str(time.time())
 
 # 增加图书
-def add():
+def add_book(id_book,name_book,author_book,type_book,publish_book,num_book):
     id_book = _get_book_id
     name_book = input('请输入书名：')
     author_book = input('请输入作者：')
     type_book = input('请输入种类：')
+    publish_book = input('请输入出版社：')
     num_book = int(input('请输入数量：'))
     global surplus_book
     surplus_book += num_book
     # 将图书信息以字典的方式存储
-    book_information = {'编号':id_book,'书名':name_book,'作者':author_book,'种类':type_book,'增加数量':num_book,'剩余数量':surplus_book}
+    ret = cache['book_list']
+    ret.append({"id":id_book,"book_name":name_book,"author":author_book,"type":type_book,"publish":publish_book,"num":num_book,"surplus":surplus_book})
+    cache['book_list'] = ret
+
+    '''global book_information
+    book_information = {'编号':id_book,'书名':name_book,'作者':author_book,'种类':type_book,'出版社':publish_book,'增加数量':num_book,'剩余数量':surplus_book}
+    '''
+
     with open("book_govering_information.txt","a",encoding="UTF-8")as f:
-        f.write(str(book_information))
+        f.write(str(cache['book_list']))
 
 
 # 删除图书
